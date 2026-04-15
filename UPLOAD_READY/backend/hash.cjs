658f4a4ff86b5bbb8fba639c530aabf28902dabe
Aakarsh914 <sahniaakarsh6@gmail.com>
2026-04-16 02:26:31 +0530

    Delete UPLOAD_READY directory

--- UPLOAD_READY/backend/hash.cjs
+++
@@ -1,4 +0,0 @@
-const bcrypt = require('bcryptjs');
-const hash = bcrypt.hashSync('hospital', 10);
-console.log(hash);
-console.log(bcrypt.compareSync('hospital', hash));
--- UPLOAD_READY/backend/src/middlewares/errorMiddleware.js
+++
@@ -1,5 +0,0 @@
-export function errorMiddleware(err, req, res, next) {
-  const status = err.status || 500;
-  const message = err.message || "Internal server error";
-  res.status(status).json({ error: message });
-}
